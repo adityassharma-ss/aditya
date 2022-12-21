509e5a65f78f4b957596c01db6c2eb794aff04f0
Aditya Sharma <adityashf7@gmail.com>
2022-12-21 13:59:57 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom";
 import ReactGA from "react-ga";
 import "./styles/index.scss";
 import App from "./App";
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
+), 
+document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById("root"));
 
 ReactGA.initialize("UA-170076691-3");
 ReactGA.pageview(window.location.pathname + window.location.search);
