190b2799ea327599f0c933f9d97e7149dfb4d5ab
Aditya Sharma <adityashf7@gmail.com>
2022-12-21 14:32:50 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from "react-dom";
 import ReactGA from "react-ga";
 import "./styles/index.scss";
 import App from "./App";
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+// eslint-disable-next-line no-unused-vars
+import {  HashRouter as Router } from "react-router-dom"
 
-ReactDOM.render((
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>
-), 
-document.getElementById('root'));
+// ReactDOM.render((<Router><App/></Router>), 
+// document.getElementById('root'));
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(
+//     <React.StrictMode>
+//       <Router>
+//         <App />
+//       </Router>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 ReactGA.initialize("UA-170076691-3");
 ReactGA.pageview(window.location.pathname + window.location.search);
