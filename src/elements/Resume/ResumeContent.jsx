de64c0682ab04387cb285a4743ce204613a9c935
Aditya Sharma <adityashf7@gmail.com>
2023-05-19 21:57:27 +0530

    Resume update

--- src/elements/Resume/ResumeContent.jsx
+++ src/elements/Resume/ResumeContent.jsx
@@ -1,7 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-no-undef */
 import React from "react";
-import MyResume from "./resume/MyResume.pdf"
 import styled from "styled-components";
 import BodyContent from "@elements/Window/BodyContent";
 
@@ -12,13 +9,11 @@ const Iframe = styled.iframe`
 `;
 
 const ResumeContent = ({ title, content }) => {
-  return (
-    <BodyContent>
-			<Iframe src={MyResume} frameBorder="0" title="VsCode"></Iframe>
-
-		</BodyContent>
-
-  );
+    return (
+        <BodyContent>
+            <Iframe src="https://drive.google.com/file/d/1wy__vQYsIlORXRCuxey2Uder6ccJc0ie/preview" frameBorder="0" title="VsCode"></Iframe>
+        </BodyContent>
+    );
 };
 
 export default ResumeContent;
